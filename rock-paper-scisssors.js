7fde9f712014575a968ecc84d9968404de1b0e48
ismaeelS <ismaeel.sheriff@gmail.com>
2026-02-09 16:53:50 -0800

    Seperates selectors and eventListeners to a new file and cleans button code

--- rock-paper-scisssors.js
+++ rock-paper-scisssors.js
@@ -7,52 +7,11 @@ const score = JSON.parse(localStorage.getItem("score")) || {
 let autoplaying = false;
 let intervalId;
 
-const pageTitle = document.querySelector(".js-page-title");
-const resultsParagraphs = document.querySelector(".js-results-text");
-
-const rockBtn = document.querySelector('.js-rock-btn');
-const paperBtn = document.querySelector('.js-paper-btn');
-const scissorsBtn = document.querySelector('.js-scissors-btn');
-const randomBtn = document.querySelector('.js-random-btn');
-const autoplayBtn = document.querySelector('.js-autoplay-btn');
-const resetScoreBtn = document.querySelector(".js-reset-score-btn");
-
-rockBtn.addEventListener("click", () => {if(!autoplaying) playGame("rock")});
-paperBtn.addEventListener("click", () => {if(!autoplaying) playGame("paper")});
-scissorsBtn.addEventListener("click", () => {if(!autoplaying) playGame("scissors")});
-randomBtn.addEventListener("click", () => {if(!autoplaying) playGame(chooseMove())});
-
-autoplayBtn.addEventListener("click", () => autoplayGame());
-resetScoreBtn.addEventListener("click", () => resetScore());
-
 const buttons = {
     "rock":rockBtn,
     "paper":paperBtn,
     "scissors":scissorsBtn
 }
-
-document.addEventListener("keyup", function(event) {
-    const keyPressed = event.key.toLowerCase();
-
-    if (keyPressed === 'r' || keyPressed === 'arrowleft') {
-        if(!autoplaying) playGame("rock");
-    }
-    else if (keyPressed === 'p' || keyPressed === 'arrowup') {
-        if(!autoplaying) playGame("paper");
-    }
-    else if (keyPressed === 's' || keyPressed === 'arrowright') {
-        if(!autoplaying) playGame("scissors");
-    }
-    else if (keyPressed === 'x' || keyPressed === 'arrowdown') {
-        if(!autoplaying) playGame(chooseMove());
-    }
-    else if (keyPressed === 'a') {
-        autoplayGame();
-    }
-    else if (keyPressed === 'backspace') {
-        resetScore();
-    }
-});
 
 updateScoreboard();
 
@@ -77,7 +36,8 @@ function chooseMove() {
  */
 function updateScoreboard() {
     document.querySelector(".js-scoreboard").innerHTML =
-    `Your record is ${score.wins} win${score.wins === 1?'':'s'}, 
+    `Your record is
+    ${score.wins} win${score.wins === 1?'':'s'}, 
     ${score.losses} loss${score.losses === 1?'':'es'}, and 
     ${score.ties} tie${score.ties === 1?'':'s'}`;
 }
@@ -87,15 +47,10 @@ function autoplayGame() {
         intervalId = setInterval(() => {
             const bothMoves = playGame(chooseMove());
 
-            if (bothMoves.includes("rock")) {
-                rockBtn.classList.add("rock-bg");
-            }
-            if (bothMoves.includes("paper")) {
-                paperBtn.classList.add("paper-bg");
-            }
-            if (bothMoves.includes("scissors")) {
-                scissorsBtn.classList.add("scissors-bg");
-            }
+            //show the move's background when it's picked during autoplay 
+            bothMoves.forEach(move => {
+                buttons[move].classList.add(`${move}-bg`);
+            });
         }, 2000);
 
         autoplaying = true;
@@ -106,7 +61,6 @@ function autoplayGame() {
 
             clearInterval(intervalId);
             autoplaying = false;
-
         }
     }
 }
