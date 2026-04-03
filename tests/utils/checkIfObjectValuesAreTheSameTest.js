a7e3127a59b4b57ceb79ca1161fa430fc6691b02
ismaeelS <ismaeel.sheriff@gmail.com>
2026-04-03 05:34:17 -0700

    moved and renamed the console log tests

--- tests/utils/checkIfObjectValuesAreTheSameTest.js
+++ tests/utils/checkIfObjectValuesAreTheSameTest.js
@@ -1,114 +1,89 @@
 import { checkIfObjectValuesAreTheSame } from "../../scripts/utils/object-helpers.js";
 
-//test suite for object comparison
+describe("test suite: object comparison", () => {
 
-//TEST ON SCORES
+    describe("test on score", () => {
+        const score001 = {"wins":0,"losses":0,"ties":1};
+        
+        const score000 = {"wins":0,"losses":0,"ties":0};
+        
+        it("notices difference between scores", () => {
+            expect(checkIfObjectValuesAreTheSame(score000, score001)).toBeFalse();
+        });
+    });
+    
+    describe("test on settings", () => {
+        const settingsBase = {"autoplayInterval":2000,"shortcuts":["?","a","r","p","s"],"askBeforeRemove":true,"showWarnings":false};
+        
+        const settingsIntervalDecreasedTo1 = {"autoplayInterval":1000,"shortcuts":["?","a","r","p","s"],"askBeforeRemove":true,"showWarnings":false};
+        
+        it("notices difference between number values from the same key", () => {
+            expect(checkIfObjectValuesAreTheSame(settingsBase, settingsIntervalDecreasedTo1)).toBeFalse();
+        });
+        
+        const settingsAskFalse = {"autoplayInterval":2000,"shortcuts":["?","a","r","p","s"],"askBeforeRemove":false,"showWarnings":false};
+        
+        it("notices difference between boolean values from the same key", () => {
+            expect(checkIfObjectValuesAreTheSame(settingsBase, settingsAskFalse)).toBeFalse();
+        });
 
-const score000 = {"wins":0,"losses":0,"ties":0};
+        const settingsMinusRPS = {"autoplayInterval":2000,"shortcuts":["?","a"],"askBeforeRemove":true,"showWarnings":false};
 
-const score001 = {"wins":0,"losses":0,"ties":1};
+        it("notices difference between array index values from the same key", () => {
+            expect(checkIfObjectValuesAreTheSame(settingsBase, settingsMinusRPS)).toBeFalse();
+        });
+    });
 
-//notices difference between scores
-if (!checkIfObjectValuesAreTheSame(score000, score001)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing the base score with a score containing one tie");
-}
+    describe("test on weapons", () => {
+        const weaponsBaseButtons = {"rock":{"beats":["scissors"],"ties":[],"shortcut":"r","button":{}},"paper":{"beats":["rock"],"ties":[],"shortcut":"p","button":{}},"scissors":{"beats":["paper"],"ties":[],"shortcut":"s","button":{}}};
+        
+        const weaponsBase = {"rock":{"beats":["scissors"],"ties":[],"shortcut":"r"},"paper":{"beats":["rock"],"ties":[],"shortcut":"p"},"scissors":{"beats":["paper"],"ties":[],"shortcut":"s"}};
+        
+        it("ignores button keys when comparing", () => {
+            expect(checkIfObjectValuesAreTheSame(weaponsBaseButtons, weaponsBase)).toBeTrue();
+        });
 
-//TEST ON SETTINGS
+        const weaponsNoRockShortcut = {"rock":{"beats":["scissors"],"ties":[]},"paper":{"beats":["rock"],"ties":[],"shortcut":"p"},"scissors":{"beats":["paper"],"ties":[],"shortcut":"s"}};
 
-const settingsBase = {"autoplayInterval":2000,"shortcuts":["?","a","r","p","s"],"askBeforeRemove":true,"showWarnings":false};
+        it("notices difference of key-value pairs (rock shortcut removed)", () => {
+            expect(checkIfObjectValuesAreTheSame(weaponsBaseButtons, weaponsNoRockShortcut)).toBeFalse();
+        });
+    });
 
-const settingsIntervalDecreasedTo1 = {"autoplayInterval":1000,"shortcuts":["?","a","r","p","s"],"askBeforeRemove":true,"showWarnings":false};
+    describe("general", () => {
+        const objectWithEveryCase1A = {
+            a: {phil: {age: 30, job: ['teacher', 'blah']}},
+            b: 2,
+            c: {foo: 2, bar: 1},
+            d: {baz: 1, bat: 2, arr: [2,3]},
+            f: function hi(){},
+            g: null,
+            h: Symbol('his'),
+            i: NaN,
+            j: Infinity,
+            k: 'hi!',
+            l: Date.now(),
+            m: Promise,
+        };
 
-//notices difference between autoplayIntervals
-if (!checkIfObjectValuesAreTheSame(settingsBase, settingsIntervalDecreasedTo1)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing default settings with settings with interval decreased to 1");
-}
+        const objectWithEveryCase1B = {
+            a: {phil: {age: 30, job: ['teacher', 'blah']}},
+            b: 2,
+            c: {foo: 2, bar: 1},
+            d: {baz: 1, bat: 2, arr: [2,3]},
+            f: function hi(){},
+            g: null,
+            h: Symbol('his'),
+            i: NaN,
+            j: Infinity,
+            k: 'hi!',
+            l: Date.now(),
+            m: Promise,
+        };
 
-const settingsAskFalse = {"autoplayInterval":2000,"shortcuts":["?","a","r","p","s"],"askBeforeRemove":false,"showWarnings":false};
+        it("is able to compare all types", () => {
+            expect(checkIfObjectValuesAreTheSame(objectWithEveryCase1A, objectWithEveryCase1B)).toBeTrue();
+        });
 
-//notices difference between nested booleans
-if (!checkIfObjectValuesAreTheSame(settingsBase, settingsAskFalse)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing default settings with settings with askBeforeRemove set to false");
-}
-
-const settingsMinusRPS = {"autoplayInterval":2000,"shortcuts":["?","a"],"askBeforeRemove":true,"showWarnings":false};
-
-//notices difference between nested arrays
-if (!checkIfObjectValuesAreTheSame(settingsBase, settingsMinusRPS)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing default settings with settings with no rps shortcuts removed");
-}
-
-//TEST OF WEAPONS
-
-const weaponsBaseButtons = {"rock":{"beats":["scissors"],"ties":[],"shortcut":"r","button":{}},"paper":{"beats":["rock"],"ties":[],"shortcut":"p","button":{}},"scissors":{"beats":["paper"],"ties":[],"shortcut":"s","button":{}}};
-
-const weaponsBase = {"rock":{"beats":["scissors"],"ties":[],"shortcut":"r"},"paper":{"beats":["rock"],"ties":[],"shortcut":"p"},"scissors":{"beats":["paper"],"ties":[],"shortcut":"s"}};
-
-//ignores button keys when comparing
-if (checkIfObjectValuesAreTheSame(weaponsBaseButtons, weaponsBase)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing weapons object with button entries with weapons object without buttons entries");
-}
-
-const weaponsNoRockShortcut = {"rock":{"beats":["scissors"],"ties":[]},"paper":{"beats":["rock"],"ties":[],"shortcut":"p"},"scissors":{"beats":["paper"],"ties":[],"shortcut":"s"}};
-
-//notices difference of existing nested key-value pairs
-if (!checkIfObjectValuesAreTheSame(weaponsBaseButtons, weaponsNoRockShortcut)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing base weapons object with base weapons object but without a shortcut for rock");
-}
-
-//TEST ON GENERAL
-
-const objectWithEveryCase1A = {
-    a: {phil: {age: 30, job: ['teacher', 'blah']}},
-    b: 2,
-    c: {foo: 2, bar: 1},
-    d: {baz: 1, bat: 2, arr: [2,3]},
-    f: function hi(){},
-    g: null,
-    h: Symbol('his'),
-    i: NaN,
-    j: Infinity,
-    k: 'hi!',
-    l: Date.now(),
-    m: Promise,
-};
-const objectWithEveryCase1B = {
-    a: {phil: {age: 30, job: ['teacher', 'blah']}},
-    b: 2,
-    c: {foo: 2, bar: 1},
-    d: {baz: 1, bat: 2, arr: [2,3]},
-    f: function hi(){},
-    g: null,
-    h: Symbol('his'),
-    i: NaN,
-    j: Infinity,
-    k: 'hi!',
-    l: Date.now(),
-    m: Promise,
-};
-
-//is able to compare all types
-if (checkIfObjectValuesAreTheSame(objectWithEveryCase1A, objectWithEveryCase1B)) {
-    console.log("passed");
-}
-else {
-    console.log("failed comparing identical complex ojects");
-}
+    });
+});
